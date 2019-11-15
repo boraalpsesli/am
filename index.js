f2a68260576d1cde2721dc270041f76444f3f68d
Bora Alp Sesli <boraalpsesli@hotmail.com>
2019-11-15 22:26:14 +0300

    logger Implemented

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 
 const App = express();
+const logger = (req, res, next) => {
+  console.log(`${req.protocol}://${req.get("host")}${req.originalUrl}`);
+  next();
+};
 
+App.use(logger);
 App.get("/", (req, res) => res.send("<h1>Animatrix-Server</h1>"));
 
 App.listen(5000, () => console.log("Server started on  port 50000"));
