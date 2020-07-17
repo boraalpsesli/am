43677e254e0dd0a0795a1ef6c5c779818150dcd2
kittydoor <me@kitty.sh>
2020-07-17 17:32:29 +0200

    Fixed all errors/warnings from eslint

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const mongoose = require("mongoose")
+const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 
-const logger = require("./middleware/logger");
-const AuthRouter = require("./routes/auth");
-const UserRouter = require("./routes/user");
+const logger = require('./middleware/logger');
+const AuthRouter = require('./routes/auth');
+const UserRouter = require('./routes/user');
 
 dotenv.config();
 const startserver = async () => {
@@ -15,16 +15,16 @@ const startserver = async () => {
   App.use(cors());
   App.use(logger);
   App.use(express.json());
-  App.use(express.urlencoded({ extended: false }));
+  App.use(express.urlencoded({extended: false}));
 
-  App.get("/", (req, res) => res.send("<h1>Animatrix-Server</h1>"));
-  App.use("/auth", AuthRouter);
-  App.use("/user", UserRouter);
+  App.get('/', (req, res) => res.send('<h1>Animatrix-Server</h1>'));
+  App.use('/auth', AuthRouter);
+  App.use('/user', UserRouter);
   await mongoose.connect(process.env.DB_CONNECT, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   });
 
-  App.listen(5000, () => console.log("Server started on  port 50000"));
+  App.listen(5000, () => console.log('Server started on  port 50000'));
 };
 startserver();
